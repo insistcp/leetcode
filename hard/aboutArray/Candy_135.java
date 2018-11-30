8b093ba371c2e414e72b2ff51275e097ba656aca
chenpeng <764762964@qq.com>
2018-11-30 12:02:48 +0800

    优化代码

--- hard/aboutArray/Candy_135.java
+++ hard/aboutArray/Candy_135.java
@@ -1,4 +1,103 @@
 package hard.aboutArray;
 
+/**
+* @Description:  给定一个数组，数组中元素代表该位置的小朋友获取糖果的概率，要求相邻位置的小朋友概率高的人获取的糖果数更多；
+ * 现在问题最少需要多少个糖果才能满足所有小朋友的分配要求。
+* @Author: chenpeng
+* @Date: 11:48 2018/11/30
+* @param:
+* @return:
+*/
 public class Candy_135 {
+    /***
+     *  找到最小概率的小朋友，然后依次采用贪心算法求解
+     * @param ratings
+     * @return
+     */
+    public int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) {
+            return 0;
+        }
+        int len = ratings.length;
+        int[] dp = new int[len];
+        int minIndex = 0;
+        for (int i = 1;i<len;i++) {
+            if (ratings[i]<ratings[minIndex]) {
+                minIndex = i;
+            }
+        }
+        dp[minIndex] = 1;
+        fillDPUp(dp,ratings,minIndex+1);
+        fillDPDown(dp,ratings,minIndex-1);
+        int sum = 0;
+        for (int i =0;i<len;i++) {
+            sum+=dp[i];
+        }
+        return sum;
+    }
+    public int fillDPDown(int[] dp,int[] rating,int start) {
+        if (start < 0 || start>=dp.length) {
+            return 0;
+        } else {
+            while (true) {
+                if (rating[start] > rating[start+1]) {
+                    dp[start] = dp[start + 1] + 1;
+                } else {
+                    if (dp[start+1] == 1  && rating[start] != rating[start+1]) {
+                        return 1;
+                    } else {
+                        dp[start] = 1;
+                    }
+                }
+                int returnV = fillDPDown(dp,rating,start-1);
+                if ( returnV == 1) {
+                    int tmpIndex = start;
+                    dp[start] = dp[start]+1;
+                    while (start<dp.length && rating[start+1]>rating[start] && dp[start+1]<=dp[start]) {
+                        dp[start+1] = dp[start+1]+1;
+                        start++;
+                    }
+                    start = tmpIndex-1;
+                } else {
+                    return 0;
+                }
+            }
+        }
+    }
+    public int fillDPUp(int[] dp,int[] rating,int start) {
+        if (start <= 0 || start>=dp.length) {
+            return 0;
+        } else {
+            while (true) {
+                if (rating[start] > rating[start-1]) {
+                    dp[start] = dp[start - 1] + 1;
+                }  else {
+                    if (dp[start-1] == 1 && rating[start] != rating[start-1]) {
+                        return 1;
+                    } else {
+                        dp[start] = 1;
+                    }
+                }
+                int returnV = fillDPUp(dp,rating,start+1);
+                if ( returnV == 1) {
+                    int tmpIndex = start;
+                    dp[start] = dp[start]+1;
+                    while (start-1>=0 && rating[start-1]>rating[start] && dp[start-1] <= dp[start]) {
+                        dp[start-1] = dp[start-1]+1;
+                        start--;
+                    }
+                    start = tmpIndex+1;
+                } else {
+                    return 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Candy_135 candy_135 = new Candy_135();
+        int[] candy = {1,3,2,2,1};
+        int res = candy_135.candy(candy);
+        System.out.println(res);
+    }
 }
--- middle/aboutArray/GasStation_134.java
+++ middle/aboutArray/GasStation_134.java
@@ -1,4 +1,49 @@
 package middle.aboutArray;
-
+/**
+* @Description:  给定两个数组gas 和cost ,其中gas代表每一个加油站现有的油量，cost代表从当前加油站到下一个加油站需要消耗的油量；
+ * 现在问题从哪一个位置出发可以走遍所有加油站
+* @Author: chenpeng
+* @Date: 10:08 2018/11/30
+* @param:
+* @return:
+*/
 public class GasStation_134 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if (gas == null ||gas.length==0)
+        {
+            return 0;
+        }
+        int len = gas.length;
+
+        for (int i = 0;i<len;i++) {
+            int pace = 1;
+            int hasGas = 0;
+            int start = i;
+            while (pace<=len) {
+                hasGas+= gas[start];
+                int mCost = cost[start];
+                if (hasGas<mCost) {
+                    break;
+                }
+                hasGas-=mCost;
+                start++;
+                if (start == len) {
+                    start = 0;
+                }
+                pace++;
+            }
+            if (pace > len) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        GasStation_134 gasStation_134 = new GasStation_134();
+        int[] gas = {1,2,3,4,5};
+        int[] cost = {3,4,5,1,2};
+        int res = gasStation_134.canCompleteCircuit(gas, cost);
+        System.out.println(res);
+    }
 }
